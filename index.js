fddec45c428fbb58a77a796b89d3a1125bd1f905
mohamed_alkalfat <mh.alkalfat99@gmail.com>
2023-04-12 13:32:44 +0200

    add api for the one img

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ window.onscroll = function(){
 
     
 }
+
+var img = document.getElementById("img")
+
+var ajax = new XMLHttpRequest;
+
+ajax.open("GET","https://api.themoviedb.org/3/movie/550?api_key=848a58c1ebc5d309cf08289753720cf9")
+
+ajax.send()
+
+ajax.onreadystatechange = function(){
+    if(ajax.readyState == 4){
+      var aa =  JSON.parse(ajax.response)
+       img.onclick =function(){console.log(aa)} 
+    }
+}
